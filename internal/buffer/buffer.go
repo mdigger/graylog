bcce048f730daa44c8f5273906a058fc2ccb7557
Dmitry Sedykh <sedykh@gmail.com>
2022-12-14 21:53:44 +0300

    doc: comments

--- internal/buffer/buffer.go
+++ internal/buffer/buffer.go
@@ -1,8 +1,4 @@
-// Copyright 2022 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// Package buffer provides a pool-allocated byte buffer.
+// Package buffer provides a pool-allocated byte buffer with custom methods.
 package buffer
 
 import (
